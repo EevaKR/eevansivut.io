8a2b9b3744b0084890863c2a9c8c007f8c33be0c
EevaKR <127771026+EevaKR@users.noreply.github.com>
2023-09-26 14:53:43 +0300

    Update script.js

--- assetts/script.js
+++ assetts/script.js
@@ -1,1 +1,15 @@
-a
+
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Jotain tekstiä ilmestyy";
+}
+
+function myFunction(imgs) {
+  
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
